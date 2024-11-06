c7e772a168d41046748327b76db84513dbf68054
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-06 12:19:49 +0700

    move styling to external css file

--- 3.use_framework_Express_js/starting-app/routes/admin.js
+++ 3.use_framework_Express_js/starting-app/routes/admin.js
@@ -1,17 +1,20 @@
+const path = require('path');
+
 const express = require('express');
-const path = require('path'); 
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
-const rootDir = require('../utils/path');
 
-router.get('/add-product',(req, res, next) => {
-    /* res.send('<form action="/admin/add-product" method="POST"> <input type="text" name="product"><button type="submit">submit</button></input></form>'); */
-    res.sendFile(path.join(rootDir,'views','add-product.html'))
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-product',(req,res,next) =>{
-    console.log(req.body);
-    res.redirect('/')
-})
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
