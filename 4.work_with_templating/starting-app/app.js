1d3ab11dc687de66966d580495243607b14a836c
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-12 21:21:39 +0700

    finished pug ttem

--- 4.work_with_templating/starting-app/app.js
+++ 4.work_with_templating/starting-app/app.js
@@ -6,19 +6,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', 'views')
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
