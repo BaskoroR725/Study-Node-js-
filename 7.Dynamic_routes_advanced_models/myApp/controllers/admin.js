ad8007b13b5e0a5a4bcbd92fbf95be98af6bb4dc
Baskoro Ramadhan <baskoror725@gmail.com>
2024-12-06 22:17:38 +0700

    using query params

--- 7.Dynamic_routes_advanced_models/myApp/controllers/admin.js
+++ 7.Dynamic_routes_advanced_models/myApp/controllers/admin.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
+    /* formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true */
   });
 };
 
@@ -19,6 +19,23 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode){
+    return res.redirect('/');
+  }
+
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    editing: editMode
+    /* formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true */
+  });
+};
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
